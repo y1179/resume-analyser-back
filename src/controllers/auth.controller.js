08a6aa0b7c17f1b16844f1f43a0e2525cc05f5a9
Yas Patle <you@example.com>
2026-03-29 20:24:04 +0530

    S

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,7 +42,11 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None"
+});
 
 
     res.status(201).json({
@@ -88,7 +92,11 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None"
+});
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
